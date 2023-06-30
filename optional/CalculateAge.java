f4b473b9b9adec70792e342495ec0dd25e00024e
rishi <69199334+konadrishi@users.noreply.github.com>
2023-06-30 14:49:16 -0500

    Add files via upload

---
+++ optional/CalculateAge.java
@@ -0,0 +1,14 @@
+
+import java.time.LocalDate;
+import java.time.Period;
+
+public class CalculateAge {
+
+    public static void main(String[] args) {
+        LocalDate currentDate = LocalDate.now();
+        LocalDate birthdate = LocalDate.of(1999, 01, 6);
+        Period age = Period.between(birthdate, currentDate);
+        System.out.println(age.getYears());
+    }
+
+}
